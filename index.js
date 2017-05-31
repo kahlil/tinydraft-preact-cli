79da740ef27dae177af977f25d6cb5bddbf45b60
Kahlil Lechelt <hello@kahlil.info>
2017-05-31 16:08:11 +0200

    try a ting with Hoodie

--- index.js
+++ index.js
@@ -3,6 +3,26 @@ import './style';
 import { h } from 'preact';
 import { Router } from 'preact-router';
 
+import Hoodie from '@hoodie/client';
+import PouchDB from 'pouchdb';
+
+const hoodie = new Hoodie({
+  url: 'http://localhost:9999',
+  PouchDB: PouchDB
+});
+
+// console.log(PouchDB);
+// hoodie.account.signUp({
+//   username: 'pat@Example.com',
+//   password: 'secret'
+// }).then(function (accountAttributes) {
+//   hoodie.log.info('Signed up as %s', accountAttributes.username)
+// }).catch(function (error) {
+//   hoodie.log.error(error)
+// })
+
+// console.log(hoodie)
+
 // import Header from './components/header';
 import Home from './routes/home';
 import New from './routes/new';
